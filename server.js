1556992fe64dc4a59a00ad499fe84703f1a2a12a
eyad-mansour <eyad.mansour.1997@gmail.com>
2022-08-05 04:01:51 +0300

    finished all cards for lab7

---
+++ server.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const express = require("express");
+
+require("dotenv").config();
+
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const weatherData = require("./data/weather.json");
+const response = require("express");
+
+app.get("/weather", (req, res) => {
+  const searchQuery = req.query.searchQuery;
+  const lat = req.query.lat;
+  const lon = req.query.lon;
+
+  const cityArr = weatherData.find(
+    (item) => item.city_name.toLowerCase() === searchQuery.toLowerCase()
+  );
+
+  try {
+    const cityData = cityArr.data.map((item) => new Forcast(item));
+
+    res.status(200).send(cityData);
+  } catch (error) {
+    errorHandeler(error, res);
+  }
+
+  //   console.log(cityArr);
+  // res.send({ cityArr });
+});
+
+// app.get("*", (req, res) => {
+//   res.status(404).send("page not found");
+// });
+
+function errorHandeler(error, res) {
+  res.status(500).send({ error: "something went wrong" });
+}
+
+class Forcast {
+  constructor(day) {
+    this.date = day.valid_date;
+    this.description = day.weather.description;
+  }
+}
+
+app.listen(process.env.PORT || 3002, () => {
+  console.log("server is working");
+});
